8a3e4dc101f8cc85e6df26fd5817a7e4f045f2cd
Apis312 <162021952+Apis312@users.noreply.github.com>
2024-05-05 11:28:41 +0200

    Add files via upload

--- company/abstracts/Employee.java
+++ company/abstracts/Employee.java
@@ -6,6 +6,7 @@ public abstract class Employee implements Employable{
 	private String name;
 	private double salary;
 	private int id;
+	private String hireDate;
 	
 	public Employee(String n, double s, int i){
 		System.out.println("Konstruktor employee");
@@ -19,10 +20,16 @@ public abstract class Employee implements Employable{
 	public void setName(String n) {this.name = n;}
 	public void setSalary(double s) {this.salary = s;}
 	public void setId(int i) {this.id = i;}
+	public void setHireDate(String d) { this.hireDate = d;}
 	
 	public String getName() {return this.name;}
 	public double getSalary() {return this.salary;}
-	public int getId() {return this.id;}
+	public int hashCode() {return this.id;}
+	public String getHireDate() {return this.hireDate;}
+	
+	
+	public String getPosition() {return "" + this.getClass().getSimpleName();}
+	
 	
 	
 	
